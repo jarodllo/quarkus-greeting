11a8c6de485a09ed3930da9e180f90f64feceb88
Javi Rodriguez <javierrodriguezllorente@mrjeffapp.com>
2019-04-04 23:19:18 +0200

    First commit

---
+++ src/main/java/org/acme/quickstart/Account.java
@@ -0,0 +1,27 @@
+package org.acme.quickstart;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    private Long id;
+    private String name;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
